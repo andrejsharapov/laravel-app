44c6dde9e2f9da67d7105b92fc6ef5bc36c8fde8
Andrej Sharapov <an.sharapov90@gmail.com>
2022-11-18 00:10:26 +0300

    chore(dev): add default font and other links

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -6,8 +6,16 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('meta.title') &mdash; {{ config('app_config.app.app_name') }}</title>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Open+Sans&display=swap" rel="stylesheet">
+
   <script src="https://cdn.tailwindcss.com"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Open+Sans&display=swap" rel="stylesheet">
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+  <style>
+    body {
+      font-family: Poppins, sans-serif;
+    }
+  </style>
 
   @yield('styles')
 
