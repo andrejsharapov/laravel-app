0d556cbde39ca91920ac27730e08412658be2c1f
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-18 14:04:12 +0300

    job(dev): add new paths

--- database_/appbar.php
+++ database_/appbar.php
@@ -1,34 +1,49 @@
 <?php
 
 return [
-  [
-    'label' => config('app_config.app.app_name'),
-    'path' => '/',
-    'blank' => false,
-  ], [
-    'label' => 'PHP docs',
-    'path' => 'https://www.php.net/manual/ru/language.basic-syntax.phpmode.php',
-    'blank' => true,
-  ], [
-    'label' => 'Resources',
-    'path' => [
-      [
-        'label' => 'Tailwind CSS',
-        'path' => 'https://tailwindcss.com',
+    [
+        'label' => config('app_config.app.app_name'),
+        'path' => '/',
+        'blank' => false,
+    ], [
+        'label' => 'Pages',
+        'path' => [
+            [
+                'label' => 'Projects',
+                'path' => '/projects',
+                'blank' => false,
+            ],
+//            [
+//                'label' => '',
+//                'path' => '#',
+//                'blank' => true,
+//            ]
+        ]
+    ],
+    [
+        'label' => 'PHP docs',
+        'path' => 'https://www.php.net/manual/ru/language.basic-syntax.phpmode.php',
+        'blank' => true,
+    ], [
+        'label' => 'Resources',
+        'path' => [
+            [
+                'label' => 'Tailwind CSS',
+                'path' => 'https://tailwindcss.com',
+                'blank' => true,
+                // ], [
+                //   'label' => '',
+                //   'path' => '#',
+                //   'blank' => true,
+            ],
+        ],
+    ], [
+        'label' => 'GitHub',
+        'path' => 'https://github.com/andrejsharapov/sf-php',
         'blank' => true,
         // ], [
-        //   'label' => '',
+        //   'label' => 'test',
         //   'path' => '#',
         //   'blank' => true,
-      ],
     ],
-  ], [
-    'label' => 'GitHub',
-    'path' => 'https://github.com/andrejsharapov/sf-php',
-    'blank' => true,
-    // ], [
-    //   'label' => 'test',
-    //   'path' => '#',
-    //   'blank' => true,
-  ],
 ];
