fe654e11276652ea1fb72862ad5c4f622df61e82
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-18 13:45:04 +0300

    job(dev): start using controller

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProjectsController extends Controller
+{
+
+    public function getContent()
+    {
+        return view('pages/projects');
+    }
+}
