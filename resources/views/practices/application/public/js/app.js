726cbbcf735b642238da50aac74a198b4edd8d16
Andrej Sharapov <an.sharapov90@gmail.com>
2022-11-23 20:17:06 +0300

    chore(refactor): bring to a unified style

--- resources/views/practices/application/public/js/app.js
+++ resources/views/practices/application/public/js/app.js
@@ -1,8 +1,8 @@
 const btn = document.querySelector('.btn > span');
 
 if (btn) {
-    btn.addEventListener('click', () => {
-        btn.innerHTML =
-            (btn.innerHTML === 'Thx!') ? btn.innerHTML = 'Clap more' : btn.innerHTML = 'Thx!';
-    })
+  btn.addEventListener('click', () => {
+    btn.innerHTML =
+      (btn.innerHTML === 'Thx!') ? btn.innerHTML = 'Clap more' : btn.innerHTML = 'Thx!';
+  })
 }
