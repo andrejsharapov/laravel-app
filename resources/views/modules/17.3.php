42ecf511d05ebe5134b2b0c35568170b81836940
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-23 12:54:00 +0300

    chore(dev): create sql tables

--- resources/views/modules/17.3.php
+++ resources/views/modules/17.3.php
@@ -1,13 +1,22 @@
-<!--<pre><code class="language-sql">-->
-<!--</code></pre>-->
-
-<pre><code class="language-css">
-</code></pre>
-
-<pre><code class="language-js">
+<pre><code class="language-sql">
+--17.3.1
+CREATE TABLE clients (
+  id SERIAL PRIMARY KEY, 
+  name character varying(200) NOT NULL, 
+  phone character varying(30) NOT NULL, 
+  email character varying(200) NOT NULL
+);
 </code></pre>
 
 <pre><code class="language-sql">
+INSERT INTO clients (name, phone, email) VALUES ('', '', '');
+INSERT INTO clients (name, phone, email) VALUES ('', '', '');
+INSERT INTO clients (name, phone, email) VALUES ('', '', '');
+INSERT INTO clients (name, phone, email) VALUES ('', '', '');
+INSERT INTO clients (name, phone, email) VALUES ('', '', '');
+INSERT INTO clients (name, phone, email) VALUES ('', '', '');
+INSERT INTO clients (name, phone, email) VALUES ('', '', '');
+INSERT INTO clients (name, phone, email) VALUES ('', '', '');
 </code></pre>
 
 <pre><code class="language-sql">
@@ -19,6 +28,17 @@ CREATE TABLE products (
   category VARCHAR(100),
   in_stock INT default 0
 );</code></pre>
+
+<pre><code class="language-sql">
+INSERT INTO clients (name, price, category, in_stock) VALUES ('', 1000, '', 5);
+INSERT INTO clients (name, price, category, in_stock) VALUES ('', 1000, '', 5);
+INSERT INTO clients (name, price, category, in_stock) VALUES ('', 1000, '', 5);
+INSERT INTO clients (name, price, category, in_stock) VALUES ('', 1000, '', 5);
+INSERT INTO clients (name, price, category, in_stock) VALUES ('', 1000, '', 5);
+INSERT INTO clients (name, price, category, in_stock) VALUES ('', 1000, '', 5);
+INSERT INTO clients (name, price, category, in_stock) VALUES ('', 1000, '', 5);
+INSERT INTO clients (name, price, category, in_stock) VALUES ('', 1000, '', 5);
+</code></pre>
 
 <pre><code class="language-sql">
 --17.3.3
@@ -33,6 +53,17 @@ CREATE TABLE orders (
 );</code></pre>
 
 <pre><code class="language-sql">
+INSERT INTO clients (client_id, date, status, address) VALUES (1, '', '', '');
+INSERT INTO clients (client_id, date, status, address) VALUES (1, '', '', '');
+INSERT INTO clients (client_id, date, status, address) VALUES (1, '', '', '');
+INSERT INTO clients (client_id, date, status, address) VALUES (1, '', '', '');
+INSERT INTO clients (client_id, date, status, address) VALUES (1, '', '', '');
+INSERT INTO clients (client_id, date, status, address) VALUES (1, '', '', '');
+INSERT INTO clients (client_id, date, status, address) VALUES (1, '', '', '');
+INSERT INTO clients (client_id, date, status, address) VALUES (1, '', '', '');
+</code></pre>
+
+<pre><code class="language-sql">
 --17.3.4
 CREATE TABLE positions (
   id SERIAL,
@@ -43,3 +74,14 @@ CREATE TABLE positions (
   CONSTRAINT order_fk FOREIGN KEY (order_id) REFERENCES orders(id),
   CONSTRAINT product_fk FOREIGN KEY (product_id) REFERENCES products(id)
 );</code></pre>
+
+<pre><code class="language-sql">
+INSERT INTO clients (order_id, product_id, amount) VALUES (1, 2, 3);
+INSERT INTO clients (order_id, product_id, amount) VALUES (1, 2, 3);
+INSERT INTO clients (order_id, product_id, amount) VALUES (1, 2, 3);
+INSERT INTO clients (order_id, product_id, amount) VALUES (1, 2, 3);
+INSERT INTO clients (order_id, product_id, amount) VALUES (1, 2, 3);
+INSERT INTO clients (order_id, product_id, amount) VALUES (1, 2, 3);
+INSERT INTO clients (order_id, product_id, amount) VALUES (1, 2, 3);
+INSERT INTO clients (order_id, product_id, amount) VALUES (1, 2, 3);
+</code></pre>
