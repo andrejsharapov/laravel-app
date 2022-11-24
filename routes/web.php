0d7a4e9834b9e18f482df1f9aa649dbae9cd6d40
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-24 11:57:51 +0300

    chore(dev): add routes to practical works

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+// home page
 Route::get('/', function () {
   return view('pages/welcome');
 });
 
-// using controller
+// other pages with a controller
 Route::get('/modules', 'ModulesController@getContent');
 Route::get('/page', 'PagesController@getContent');
+
+// routes for practical works
+Route::get('practices/spa', function () {
+  return view('practices/spa/login');
+});
+Route::get('practices/application', function () {
+  return view('practices/application/index');
+});
 
