19bcbba30e4ab981c3fffb71a57c4b9ce178304b
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-24 12:20:42 +0300

    chore(dev): add sql content and tasks

--- resources/views/modules/19.2.php
+++ resources/views/modules/19.2.php
@@ -1,11 +1,30 @@
-<!--<pre><code class="language-sql">-->
-<!--</code></pre>-->
-
-<pre><code class="language-css">
-</code></pre>
-
-<pre><code class="language-js">
-</code></pre>
-
 <pre><code class="language-sql">
+/**
+ *
+ * CREATE INDEX
+ * CREATE UNIQUE INDEX
+ *
+ */
+--  
+CREATE INDEX new_index ON clients (name, phone);
+
+CREATE UNIQUE INDEX email_unique_key ON clients (lower(email));
+
+-- 19.2.2
+CREATE INDEX category_price_index ON products(category, price);
+
+-- 19.2.3
+CREATE INDEX order_index_date_by_desc ON orders(date DESC NULLS FIRST);
+
+-- use
+SELECT
+  *
+FROM
+  clients
+WHERE
+  name = 'Бук Василий Петрович'
+  AND phone = '8999233444';
+
+-- 
+DROP INDEX new_index;
 </code></pre>
