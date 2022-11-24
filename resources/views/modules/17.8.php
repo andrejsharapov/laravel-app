58dbe04cef816c4dbc9b77a65fb6a6966e9695e7
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-24 12:14:34 +0300

    chore(dev): add sql content and tasks

--- resources/views/modules/17.8.php
+++ resources/views/modules/17.8.php
@@ -1,11 +1,48 @@
-<!--<pre><code class="language-sql">-->
-<!--</code></pre>-->
-
-<pre><code class="language-css">
-</code></pre>
-
-<pre><code class="language-js">
-</code></pre>
-
 <pre><code class="language-sql">
+/**
+ * 
+ * UNION	-- обьединение
+ * INTERSECT	-- перечисление
+ * EXCEPT	-- исключение
+ * 
+ * ON CONFLICT DO NOTHING -- В СЛУЧАЕ ОШИБКИ НЕ ДЕЛАЙ НИЧЕГО
+ * 
+ */
+-- 17.8
+SELECT
+  id
+FROM
+  orders
+union
+SELECT
+  id
+FROM
+  orders
+WHERE
+  status = 'done';
+
+SELECT
+  id
+FROM
+  orders
+intersect
+SELECT
+  id
+FROM
+  orders
+WHERE
+  status = 'done';
+
+SELECT
+  id
+FROM
+  orders
+except
+SELECT
+  id
+FROM
+  orders
+WHERE
+  status = 'done';
+  
 </code></pre>
