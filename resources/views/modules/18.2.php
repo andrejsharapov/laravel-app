1492e7276f9f153bdc8b8d2c04c8154a90cd7a42
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-24 12:17:21 +0300

    chore(dev): add sql content and tasks

--- resources/views/modules/18.2.php
+++ resources/views/modules/18.2.php
@@ -1,11 +1,25 @@
-<!--<pre><code class="language-sql">-->
-<!--</code></pre>-->
-
-<pre><code class="language-css">
-</code></pre>
-
-<pre><code class="language-js">
-</code></pre>
-
 <pre><code class="language-sql">
+/**
+ * 
+ * pg_catalog
+ * .pg_class
+ * .pg_attribute
+ *
+ */
+-- 18.2.1
+SELECT
+  *
+FROM
+  pg_catalog.pg_class
+WHERE
+  relname IN ('orders', 'clients', 'products');
+
+-- 18.2.2
+SELECT
+  *
+FROM
+  pg_catalog.pg_attribute
+WHERE
+  attrelid = 16404;
+  
 </code></pre>
