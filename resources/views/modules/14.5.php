0d8d89bd38e8aaf1881558d86218cd9f37425799
Andrej Sharapov <an.sharapov90@gmail.com>
2022-11-27 20:23:49 +0300

    chore(dev): add new block

--- resources/views/modules/14.5.php
+++ resources/views/modules/14.5.php
@@ -5,4 +5,10 @@
 </p>
 
 <pre><code class="language-php">
+var_dump($_FILES['fileToUpload']);
+
+if (isset($_FILES['fileToUpload']) && UPLOAD_ERR_OK === $_FILES['fileToUpload']['error']) {
+  echo 'Файл ' . $_FILES['fileToUpload']['name'] . ' успешно загружен на сервер!';
+}
+
 </code></pre>
