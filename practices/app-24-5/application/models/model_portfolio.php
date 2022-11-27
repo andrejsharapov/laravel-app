b898325824560c4f82e473ac36514826d66e3c4c
Andrej Sharapov <an.sharapov90@gmail.com>
2022-11-27 21:22:17 +0300

    chore(dev): update apps

---
+++ practices/app-24-5/application/models/model_portfolio.php
@@ -0,0 +1,12 @@
+<?php
+
+class Model_Portfolio extends Model
+{
+  /**
+   * @return string
+   */
+  public function get_data(): string
+  {
+    return 9;
+  }
+}
---
+++ practices/app-24-5/public/js/app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn > span');
+
+if (btn) {
+  btn.addEventListener('click', () => {
+    btn.innerHTML =
+      (btn.innerHTML === 'Thx!') ? btn.innerHTML = 'Clap more' : btn.innerHTML = 'Thx!';
+  })
+}
