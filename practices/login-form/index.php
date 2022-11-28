a17984d8067ff7cbf3a5951a8b850284af383c89
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-28 10:00:56 +0300

    chore(fix): change input id

--- practices/login-form/index.php
+++ practices/login-form/index.php
@@ -15,7 +15,7 @@
 
   <label for="password">
     <input
-      id="login"
+      id="password"
       class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
       name="password"
       type="password"
