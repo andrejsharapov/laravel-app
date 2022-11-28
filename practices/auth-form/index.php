27b22fd937a5f3956bbeea30f1633a59c5cb4aea
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-28 09:59:36 +0300

    chore(dev): add label binding

--- practices/auth-form/index.php
+++ practices/auth-form/index.php
@@ -3,8 +3,9 @@
   action="process.php"
   class="w-full mx-auto max-w-sm flex flex-col gap-5"
 >
-  <label>
+  <label for="login">
     <input
+      id="login"
       class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
       name="login"
       type="text"
@@ -12,8 +13,9 @@
     >
   </label>
 
-  <label>
+  <label for="password">
     <input
+      id="login"
       class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
       name="password"
       type="password"
