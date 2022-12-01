e8b448027071227dfb2ec24927af1b3ab39632fa
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-12-01 14:27:51 +0300

    dev: move the project to a separate repo

--- practices/app-14-8/users.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-  'admin' => ['id' => '1', 'password' => '123'],
-  'moder' => ['id' => '1', 'password' => '321'],
-  'user1' => ['id' => '2', 'password' => '123'],
-  'user2' => ['id' => '3', 'password' => '456'],
-  'user3' => ['id' => '4', 'password' => '789'],
-];
