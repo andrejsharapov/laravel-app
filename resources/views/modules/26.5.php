b94faf02a7439fedbc6e0dac0c94285e4217ed7e
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2023-01-20 15:32:52 +0300

    chore(doc): add `SOLID` information

---
+++ resources/views/modules/26.5.php
@@ -0,0 +1,26 @@
+// 26.5. Принципы SOLID
+
+<p>Принципы SOLID — это набор правил, которые необходимо применять во время работы над программным обеспечением (ПО) для его улучшения.</p>
+
+<pre><code class="language-md">
+- Принцип единственной ответственности (Single Responsibility Principle),
+- Принцип открытости/закрытости (Open-closed),
+- Принцип подстановки Барбары Лисков (Liskov substitution),
+- Принцип разделения интерфейса (Interface segregation),
+- Принцип инверсии зависимостей (Dependency Invertion).
+</code></pre>
+
+<div class="text-2xl mt-4 mb-2">— The Single Responsibility Principle (Принцип единственной ответственности)</div>
+<p>Каждый класс выполняет лишь одну задачу.</p>
+
+<div class="text-2xl mt-4 mb-2">The Open Closed Principle (Принцип открытости/закрытости)</div>
+<p>Классы должны быть открыты для расширения и закрыты для модификации.</p>
+
+<div class="text-2xl mt-4 mb-2">The Liskov Substitution Principle (Принцип подстановки Барбары Лисков)</div>
+<p>Наследники должны повторять поведение родительского класса и должны вести себя без сюрпризов.</p>
+
+<div class="text-2xl mt-4 mb-2">The Interface Segregation Principle (Принцип разделения интерфейса)</div>
+<p>Много мелких интерфейсов лучше, чем один большой.</p>
+
+<div class="text-2xl mt-4 mb-2">The Dependency Inversion Principle (Принцип инверсии зависимостей)</div>
+<p>Зависимость на абстракциях, нет зависимостей на что-то конкретное.</p>
